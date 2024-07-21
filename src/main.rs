66421f1d97b668fd750a7170b7e4c0ea4b9a29ad
Duncan Dean <git@dunxen.dev>
2024-07-21 13:09:00 +0200

    Rename to marukana

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
 
 use bevy::prelude::*;
-use karakana::AppPlugin;
+use marukana::AppPlugin;
 
 fn main() -> AppExit {
 	App::new().add_plugins(AppPlugin).run()
